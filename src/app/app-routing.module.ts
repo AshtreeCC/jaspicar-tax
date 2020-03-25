edeffa66dd0bc199640d56238d3ac443f7a8ee78
Andrew Nash <andrew@QUEBEC.local>
2020-03-24 21:42:44 -0700

    feat: App - Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './auth/auth.service';
+import { IncomeComponent } from './pages/income/income.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    { path: '', redirectTo: 'income', pathMatch: 'full' },
+    { path: 'income', component: IncomeComponent, canActivate: [AuthGuard] },
+    // { path: 'expenses', component: ExpensesComponent, canActivate: [AuthGuard] },
+    // { path: 'stock', component: StockComponent, canActivate: [AuthGuard] },
+    // { path: 'vat', component: VatComponent, canActivate: [AuthGuard] },
+    // { path: 'tax', component: TaxComponent, canActivate: [AuthGuard] },
+    // { path: 'contact', component: ContactComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: 'income' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
