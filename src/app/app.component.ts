6bff5d0dde769e6edd9f790dd7d5f53531a245fd
Andrew Nash <akahadaka@gmail.com>
2020-03-31 11:45:42 -0700

    feat: App - Add logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
-import { AuthService } from './auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'jaspicar-tax';
+export class AppComponent implements OnInit {
+
+  isAuthenticated: boolean = false;
 
   constructor(
     private authService: AuthService,
-  ) {}
+  ) { }
 
-  logout() {
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated;
+  }
+
+  logout(): void {
     this.authService.logout();
   }
 }
