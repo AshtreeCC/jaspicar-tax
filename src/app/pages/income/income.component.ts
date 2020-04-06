84a372867ea9ed94a8a1cc0924308ed1b4558a0c
Andrew Nash <akahadaka@gmail.com>
2020-04-06 16:54:27 -0700

    feat: Income - Update table presentation

--- src/app/pages/income/income.component.ts
+++ src/app/pages/income/income.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, MatSortable } from '@angular/material/sort';
 
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, tap } from 'rxjs/operators';
 
+import { IncomeModel } from '@shared/models/income.model';
 import { DataService } from '@shared/services/data.service';
 
 import { AddIncomeComponent } from './components/add-income/add-income.component';
 import { AddExpenseComponent } from '../expenses/components/add-expense/add-expense.component';
+import { EditIncomeComponent } from './components/edit-income/edit-income.component';
+import { ConfirmDialogComponent } from '@shared/components/confirm-dialog/confirm-dialog.component';
+
+interface Total {
+  amount: number,
+  vat: number,
+  net: number,
+}
 
 @Component({
   selector: 'app-income',
@@ -21,6 +30,8 @@ import { AddExpenseComponent } from '../expenses/components/add-expense/add-expe
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IncomeComponent implements OnInit, OnDestroy {
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   private destroy$: Subject<boolean> = new Subject<boolean>();
   
@@ -36,43 +47,69 @@ export class IncomeComponent implements OnInit, OnDestroy {
   simplifiedCategories = {};
 
   displayedColumns = [
+    'index',
     'invoice', 
     'date', 
     'category', 
     'description', 
-    'vattable', 
-    'amount', 
     'vat', 
-    'net'
+    'amount', 
+    'autoVat', 
+    'autoNet',
+    'edit',
   ];
+
+  total: Total = {
+    amount: 0,
+    vat: 0,
+    net: 0,
+  };
   
   dataSource: MatTableDataSource<any> = new MatTableDataSource();
 
+  rowHoverIndex: number;
+
   constructor(
-    private dataService: DataService,
     private router: Router,
     public snackBar: MatSnackBar,
     public dialog: MatDialog,
+    public dataService: DataService,
   ) { }
 
   ngOnInit(): void {
     // Initialise date range
-    this.periodStart = new Date('2020-02-01 00:00:00');
-    this.periodUntil = new Date(this.periodStart);
-    this.periodUntil.setMonth(this.periodStart.getMonth() + 2);
+    this.onChangePeriod('2020-03-01');
+  }
 
+  onChangePeriod(start: string): void {
+    this.periodStart = new Date(start + ' 00:00:00');
+    this.periodUntil = new Date(this.periodStart);
+    this.periodUntil.setFullYear(this.periodStart.getFullYear() + 1);
+    this.periodUntil.setDate(this.periodUntil.getDate() - 1);
+    
     // Subscribe to database
+    this.destroy$.next(true);
     this.getIncomeData();
   }
 
   getIncomeData() {
     this.loading$.next(true);
-    this.dataService.getIncome(this.periodStart).pipe(
+    this.dataService.getIncome(this.periodStart, this.periodUntil).pipe(
       takeUntil(this.destroy$),
-    ).subscribe(data => {
-      this.dataSource = data;
-      this.loading$.next(false);
-    });
+    ).pipe(
+      tap(data => {
+        this.total = data.reduce((total: Total, data: IncomeModel) => {
+          total.amount += Number(data.amount);
+          total.vat += Number(data.autoVat);
+          total.net += Number(data.autoNet);
+          return total;
+        }, { amount: 0, vat: 0, net: 0 } as Total)
+      }),
+      tap(data => {
+        this.dataSource = data;
+        this.dataSource.sort = this.sort;
+      }),
+    ).subscribe( () => this.loading$.next(false) );
   }
 
   gotoExpenses() {
@@ -80,14 +117,27 @@ export class IncomeComponent implements OnInit, OnDestroy {
     const dialogRef: MatDialogRef<any> = this.dialog.open(AddExpenseComponent);
   }
 
-  openAddDialog(): void {
-    const dialogRef: MatDialogRef<any> = this.dialog.open(AddIncomeComponent);
-    
-    // dialogRef.afterClosed().subscribe(result => {
-    //   if (result) {
-        
-    //   }
-    // });
+  openDialog(action: string, income: IncomeModel = null): void {
+    let dialogRef: MatDialogRef<any>
+
+    switch(action) {
+      case 'Add': dialogRef = this.dialog.open(AddIncomeComponent, { data: income }); break;
+      case 'Edit': dialogRef = this.dialog.open(EditIncomeComponent, { data: income }); break;
+      case 'Confirm': dialogRef = this.dialog.open(ConfirmDialogComponent, { data: income }); break;
+    }
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (action === 'Confirm') {
+          this.dataService.deleteIncome(income);
+          let snackBarRef = this.snackBar.open(`Income for Invoice # ${income.invoice} removed`, '', { duration: 6000 });
+        }
+      }
+    });
+  }
+
+  onHover(i: number) {
+    this.rowHoverIndex = i + 1;
   }
 
   ngOnDestroy(): void {
