76b7d98e8061540c24de38e813de94fccd470e0a
Andrew Nash <akahadaka@gmail.com>
2020-04-06 16:48:02 -0700

    feat: App - Confirm dialog for deleting records

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onConfirm() {
+    this.dialogRef.close(true);
+  }
+
+  onCancel() {
+    this.dialogRef.close(false);
+  }
+
+}
